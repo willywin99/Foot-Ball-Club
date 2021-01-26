7590a8c6a2187f12cc81e00de0789121542fb4c4
willywin99 <willywinsianturi@gmail.com>
2021-01-26 22:25:15 +0700

    make clubs migration

---
+++ app/Http/Controllers/ClubController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Club;
+use Illuminate\Http\Request;
+
+class ClubController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Club  $club
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Club $club)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Club  $club
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Club $club)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Club  $club
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Club $club)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_01_26_151713_create_clubs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClubsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clubs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('pelatih');
+            $table->string('stadion');
+            $table->string('pemilik');
+            $table->string('lapangan_latihan');
+            $table->string('lokasi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clubs');
+    }
+}
