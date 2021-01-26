41036c2c67a4befe0ab4c09c4f8002f2ad04264b
willywin99 <willywinsianturi@gmail.com>
2021-01-26 22:41:03 +0700

    make index function

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+// $router->get('/', function () use ($router) {
+//     return $router->app->version();
+// });
+
+$router->group(['prefix' => 'api/v1', 'namespace' => 'v1'], function() use ($router) {
+
+    $router->get('/clubs', 'ClubController@index');
+
 });
